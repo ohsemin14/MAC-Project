bf15a1e6f9a42e5f1e076362fc88a0bf29b343b6
ohsemin14 <ohsemin14@gmail.com>
2026-04-15 14:14:47 +0900

    capture_pulse, data_en 등 추가

--- rtl/MAC_Controller.sv
+++ rtl/MAC_Controller.sv
@@ -6,14 +6,12 @@ parameter int Matrix_K = 8
   input logic reset_n, 
   input logic start, // 시작신호
 
-// 외부에서 전체 행렬 8x8 받아오는 2차원 포트 2개
-  input logic signed [7:0] A_mat_in [0:Array_size-1][0:Matrix_K-1],
-  input logic signed [7:0] W_mat_in [0:Array_size-1][0:Matrix_K-1],
-
   output logic valid_out,
+  output logic capture_pulse,
   output logic acc_clear_out,
-  output logic signed [7:0] a_out[0:Array_size-1],
-  output logic signed [7:0] w_out[0:Array_size-1]
+  output logic data_en, // 유효 데이터인지 0으로 덮을지 알려주는 enable 신호 -> skewing이랑 연결
+
+  output logic [2:0] mac_addr_out //M10K 메모리 읽기 주소 출력
 );
 
 localparam int MAX_Cycles = (Array_size*2) + Matrix_K;
@@ -27,20 +25,45 @@ typedef enum logic[1:0] { // 3-state
 state_t current_state, next_state; // 현재, 다음상태를 담을 변수선언
 
 logic [7:0] cycle_cnt; // 사이클 지연 신호.
+logic [2:0] mac_addr_cnt; // 주소 카운트
+logic internal_data_en;
 
 always_ff @(posedge clk or negedge reset_n) begin
   if(!reset_n) begin
     current_state <= IDLE;
     cycle_cnt <= '0;
+    mac_addr_cnt <= '0;
+    internal_data_en <= 1'b0;
   end else begin
     current_state <= next_state; // 매 클럭마다 다음상태로 업데이트
 
     if(current_state == RUN) begin // 카운터 로직
       cycle_cnt <= cycle_cnt + 1'b1;
+      if(mac_addr_cnt < Array_size-1) begin
+        mac_addr_cnt <= mac_addr_cnt + 1'b1; // 8행 데이터까지만 읽기.
+      end 
+
+      if(cycle_cnt < Array_size) begin
+        internal_data_en <= 1'b1;
+      end else begin
+        internal_data_en <= 1'b0;
+      end
+
     end else begin
       cycle_cnt <= '0;
+      mac_addr_cnt <= '0;
+      internal_data_en <= 1'b0;
     end
   end
+end
+
+// 메모리에서 오는데 클럭이 1지연되므로, 1클럭 delay해서 출력.
+always_ff @(posedge clk or negedge reset_n) begin
+    if(!reset_n) begin
+      data_en <= 1'b0;
+    end else begin
+      data_en <= internal_data_en;
+    end
 end
 
 always_comb begin
@@ -69,27 +92,20 @@ always_ff @(posedge clk or negedge reset_n)begin
   if(!reset_n)begin
     valid_out <= 1'b0;
     acc_clear_out <= 1'b0;
-    for(int i=0; i<Array_size; i++)begin
-      a_out[i] <= 8'sd0;
-      w_out[i] <= 8'sd0;
-    end
-  end else begin
+    mac_addr_out <= '0;
+    capture_pulse <= 1'b0;
+    end else begin
     valid_out <= 1'b0;
     acc_clear_out <= 1'b0;
+    capture_pulse <= 1'b0;
+    mac_addr_out <= mac_addr_cnt;
+
     if(current_state == RUN)begin
       valid_out <= 1'b1;
-    for(int i=0; i<Array_size; i++)begin
-      if(cycle_cnt>=i && cycle_cnt < i+Matrix_K)begin
-        a_out[i] <= A_mat_in[i][cycle_cnt-i]; // cycle-i 번째 데이터 주입
-        w_out[i] <= W_mat_in[i][cycle_cnt-i];
-      end else begin
-        a_out[i] <= 8'sd0;
-        w_out[i] <= 8'sd0; 
-      end
-    end
-  end else if(current_state == DONE)begin
+    end else if(current_state == DONE)begin
+      capture_pulse <= 1'b1; // DONE이 되는 순간 캡쳐 신호 출력 -> output_reg에 전달.
     acc_clear_out <= 1'b1; // 누산기 초기화
+    end
   end
-end
 end
 endmodule
